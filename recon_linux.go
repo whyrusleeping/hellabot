327f135f7aaf731cb7393aebbedc282b163bbfa8
Kristoffer Berdal <web@flexd.net>
2015-08-31 12:43:02 +0200

    Remove error check that is not needed, see https://golang.org/src/net/unixsock.go?s=826:883#L30

--- recon_linux.go
+++ recon_linux.go
@@ -45,23 +45,16 @@ func (irc *Bot) StartUnixListener() {
 
 // Attempt to hijack session previously running bot
 func (irc *Bot) hijackSession() bool {
-	unaddr, err := net.ResolveUnixAddr("unix", irc.unixastr)
-	if err != nil {
-		irc.Error("could not resolve unix socket")
-		return false
-	}
-
+	unaddr, _ := net.ResolveUnixAddr("unix", irc.unixastr) // The only way to get an error here is if the first parameter is not one of "unix", "unixgram" or "unixpacket". That will never happen.
 	con, err := net.DialUnix("unix", nil, unaddr)
 	if err != nil {
 		irc.Error("Couldnt restablish connection, no prior bot:", err)
 		return false
 	}
-
 	ncon, err := sendfd.RecvFD(con)
 	if err != nil {
 		panic(err)
 	}
-
 	netcon, err := net.FileConn(ncon)
 	if err != nil {
 		panic(err)
