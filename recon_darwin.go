c41fbcf51e0e1f3b91cb3f265cb2a0c3d57ca691
Bren Briggs <code@fraq.io>
2018-08-28 08:51:22 -0500

    Rename HijackSession() to hijackSession() for darwin and windows to avoid name collisions.

--- recon_darwin.go
+++ recon_darwin.go
@@ -3,6 +3,6 @@ package hbot
 func (irc *Bot) StartUnixListener() {}
 
 // Attempt to hijack session previously running bot
-func (irc *Bot) HijackSession() bool {
+func (irc *Bot) hijackSession() bool {
 	return false
 }
